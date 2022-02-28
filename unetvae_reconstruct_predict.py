de42912786364196137219cd38e8d332c679b6ac
trile83 <43611715+trile83@users.noreply.github.com>
2022-02-28 11:09:55 -0500

    Update unetvae_reconstruct_predict.py

--- unetvae_reconstruct_predict.py
+++ unetvae_reconstruct_predict.py
@@ -13,20 +13,15 @@ import torchvision
 from osgeo import gdal, gdal_array
 import matplotlib.pyplot as plt
 
-from unet import UNet_VAE, UNet_VAE_Softshrink_All_trainable, UNet_S
+from unet import UNet_VAE
 from unet import UNet_VAE_old, UNet_VAE_RQ_old, UNet_VAE_RQ_test, UNet_VAE_RQ_old_trainable, UNet_VAE_RQ_old_torch, UNet_VAE_RQ_new_torch, UNet_VAE_RQ_scheme3
 from unet import UNet_VAE_RQ_scheme1
 from utils.utils import plot_img_and_mask, plot_img_and_mask_3, plot_img_and_mask_recon
 
-#ground_truth_path = 'number13985.TIF'
-#ground_truth_path = 'test_img/number3731.TIF'
-#ground_truth_path = 'test_img/number32843.TIF'
-#ground_truth_path = 'test_img/number10301.TIF'
-
-image_path = 'test_img/number13458.TIF'
+image_path = './github_files/test_img/number13458.TIF'
 #image_path = 'sentinel2_im/2016002_0.tif'
 
-mask_true_path = 'test_label/number13458.TIF'
+mask_true_path = './github_files/test_label/number13458.TIF'
 use_cuda = True
 #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -163,7 +158,7 @@ def predict_img(net,
 
 def get_args():
     parser = argparse.ArgumentParser(description='Predict masks from input images')
-    parser.add_argument('--model', '-m', default='./github_checkpoints/checkpoint_unet_vae_old_epoch10_0.0_recon.pth', metavar='FILE',
+    parser.add_argument('--model', '-m', default='./github_files/github_checkpoints/checkpoint_unet_vae_old_epoch10_0.0_recon.pth', metavar='FILE',
                         help='Specify the file in which the model is stored')
     #parser.add_argument('--input', '-i', metavar='INPUT', nargs='+', default='F:\\NAIP\\256\\pa101\\test\\sat\\number13985.TIF', help='Filenames of input images', required=True)
     #parser.add_argument('--output', '-o', metavar='OUTPUT', nargs='+', default='out/predict1.tif', help='Filenames of output images')

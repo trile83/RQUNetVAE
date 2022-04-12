6ee6d6a19c1046313cd0385f61ad81bb67465663
trile83 <43611715+trile83@users.noreply.github.com>
2022-04-12 11:46:48 -0400

    Update __init__.py

--- unet/__init__.py
+++ unet/__init__.py
@@ -8,3 +8,5 @@ from .unet_vae_RQ_torch import UNet_VAE_RQ_old_torch
 from .unet_vae_RQ_torch_updated import UNet_VAE_RQ_new_torch
 from .unet_vae_RQ_scheme3 import UNet_VAE_RQ_scheme3
 from .unet_vae_RQ_scheme1 import UNet_VAE_RQ_scheme1
+from .unet_rq import UNet_RQ
+from.unet_vae_rq_activation import UNet_VAE_RQ

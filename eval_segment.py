766f274b98f4a9b7609172ab0dcc8b4ef85f6b4e
trile83 <43611715+trile83@users.noreply.github.com>
2022-04-18 10:22:52 -0400

    Update eval_segment.py

--- eval_segment.py
+++ eval_segment.py
@@ -5,8 +5,8 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    unet_jaxony_dict_file = '/home/geoint/tri/github_files/unet_jaxony_dictionary_4-5.pickle'
-    unet_vae_rq_dict_file = '/home/geoint/tri/github_files/unet_vae_RQ_dictionary_4-5.pickle'
+    unet_jaxony_dict_file = '/home/geoint/tri/github_files/unet_jaxony_dictionary_4-12.pickle'
+    unet_vae_rq_dict_file = '/home/geoint/tri/github_files/unet_vae_RQ_dictionary_4-12.pickle'
     unet_rq_dict_file = '/home/geoint/tri/github_files/unet_RQ_dictionary_4-12.pickle'
     
     # read the pickle object
@@ -29,7 +29,7 @@ if __name__ == '__main__':
         sigma_acc_unet_jaxony.append(unet_jaxony_dict[i][0]['avg_balanced_accuracy'])
         sigma_acc_unet_vae_rq.append(unet_vae_rq_dict[i][0.5]['avg_balanced_accuracy'])
         sigma_acc_unet_rq.append(unet_rq_dict[i][0.5]['avg_balanced_accuracy'])
-        #sigma_acc_unet_vae_rq_1.append(unet_vae_rq_dict[i][0.1]['avg_balanced_accuracy'])
+        sigma_acc_unet_vae_rq_1.append(unet_vae_rq_dict[i][0.1]['avg_balanced_accuracy'])
         #sigma_acc_unet_vae_rq_2.append(unet_vae_rq_dict[i][0.2]['avg_balanced_accuracy'])
         #sigma_acc_unet_vae_rq_3.append(unet_vae_rq_dict[i][0.3]['avg_balanced_accuracy'])
         #sigma_acc_unet_vae_rq_4.append(unet_vae_rq_dict[i][0.4]['avg_balanced_accuracy'])
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     sigma_acc_unet_vae_rq = np.array(sigma_acc_unet_vae_rq)
     sigma_acc_unet_rq = np.array(sigma_acc_unet_rq)
 
-    #sigma_acc_unet_vae_rq_1 = np.array(sigma_acc_unet_vae_rq_1)
+    sigma_acc_unet_vae_rq_1 = np.array(sigma_acc_unet_vae_rq_1)
     #sigma_acc_unet_vae_rq_2 = np.array(sigma_acc_unet_vae_rq_2)
     #sigma_acc_unet_vae_rq_3 = np.array(sigma_acc_unet_vae_rq_3)
     #sigma_acc_unet_vae_rq_4 = np.array(sigma_acc_unet_vae_rq_4)
@@ -57,11 +57,11 @@ if __name__ == '__main__':
     plt.title('Sigma vs Class-balanced Accuracy')
     plt.plot(sigma_range, sigma_acc_unet_jaxony, label = 'typical Unet')
     plt.fill_between(sigma_range, sigma_acc_unet_jaxony-std_unet_jaxony, sigma_acc_unet_jaxony+std_unet_jaxony, alpha=0.5)
-    plt.plot(sigma_range, sigma_acc_unet_vae_rq, label = 'Unet VAE RQ 0.5')
+    plt.plot(sigma_range, sigma_acc_unet_vae_rq, label = 'RQUnet-VAE alpha=0.5')
     plt.fill_between(sigma_range, sigma_acc_unet_vae_rq-std_unet_vae_rq, sigma_acc_unet_vae_rq+std_unet_vae_rq, alpha=0.5)
-    plt.plot(sigma_range, sigma_acc_unet_rq, label = 'Unet RQ 0.5')
-    plt.fill_between(sigma_range, sigma_acc_unet_rq-std_unet_rq, sigma_acc_unet_rq+std_unet_rq, alpha=0.5)
-    #plt.plot(sigma_range, sigma_acc_unet_vae_rq_1, label = 'Unet VAE RQ 0.1')
+    #plt.plot(sigma_range, sigma_acc_unet_rq, label = 'RQUnet alpha=0.5')
+    #plt.fill_between(sigma_range, sigma_acc_unet_rq-std_unet_rq, sigma_acc_unet_rq+std_unet_rq, alpha=0.5)
+    plt.plot(sigma_range, sigma_acc_unet_vae_rq_1, label = 'RQUNet-VAE alpha=0.1')
     #plt.plot(sigma_range, sigma_acc_unet_vae_rq_2, label = 'Unet VAE RQ 0.2')
     #plt.plot(sigma_range, sigma_acc_unet_vae_rq_3, label = 'Unet VAE RQ 0.3')
     #plt.plot(sigma_range, sigma_acc_unet_vae_rq_4, label = 'Unet VAE RQ 0.4')

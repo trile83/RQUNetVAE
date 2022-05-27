2a4b22d05ca0d80a9f94fcf01c60a71f2131965a
trile83 <43611715+trile83@users.noreply.github.com>
2022-05-27 15:07:48 -0400

    Update __init__.py

--- unet/__init__.py
+++ unet/__init__.py
@@ -1,6 +1,6 @@
 from .unet_vae_1 import UNet_VAE
 from .unet_vae_RQ import UNet_VAE_RQ_old
-from .unet_vae_rq_train_alpha import UNet_VAE_RQ_old_trainable
+from .unet_vae_rq_train_alpha import UNet_VAE_RQ_trainable
 from .unet_test import UNet_test
 from .unet_vae import UNet_VAE_old
 from .unet_vae_RQ_test_new import UNet_VAE_RQ_test
@@ -12,3 +12,4 @@ from .unet_vae_RQ_scheme2 import UNet_VAE_RQ_scheme2
 from .unet_rq import UNet_RQ
 from .unet_vae_rq_activation import UNet_VAE_RQ
 from .stacked_unet import UNet_VAE_Stacked
+from .unet_vae_update_param import UNet_VAE_update_param

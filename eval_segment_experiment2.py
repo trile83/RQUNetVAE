9d66e8eb981dbae9537590a839468ee9dff6b4f7
trile83 <43611715+trile83@users.noreply.github.com>
2022-05-27 15:07:45 -0400

    Update eval_segment_experiment2.py

--- eval_segment_experiment2.py
+++ eval_segment_experiment2.py
@@ -142,12 +142,14 @@ def predict_img(net,
     else:
         return full_mask, probs
 
-def get_f_seg(image,label):
+def get_f_seg(image, label, num_class):
 
+    image = image.reshape((256,256,3))
     f_seg = np.zeros((256,256,3))
-    y = np.zeros((256,256,3))
-    m = np.zeros((3,3)) # number of channels x number of class
-    for value in np.unique(label):
+    y = np.zeros((256,256,3)) # image after class process (normal color for class and black for non-class)
+    m = np.zeros((3,num_class)) # number of channels x number of class
+    #print('get_f_seg func: ', np.unique(label))
+    for value in range(num_class):
         itemindex = np.ma.where(label == value, 1, 0)
         for i in range(3): # number of channel
             y[:,:,i] = image[:,:,i]*itemindex
@@ -157,6 +159,7 @@ def get_f_seg(image,label):
 
 # get label_mode
 def get_label_mode(preds): # preds is the Torch tensor have 50x256x256 dim
+    preds = preds.astype(np.int64)
     label_mode = np.zeros((256,256))
     for i in range(preds.shape[1]):
         for j in range(preds.shape[2]):
@@ -196,8 +199,9 @@ def get_accuracy_map(preds, label, loop_num, img):
 
     img_name = '/home/geoint/tri/github_files/results_paper1/input_img.png'
     label_name = '/home/geoint/tri/github_files/results_paper1/groundtruth.png'
-    accu_map_name = '/home/geoint/tri/github_files/results_paper1/accuracy_map.png'
+    accu_map_name = '/home/geoint/tri/github_files/results_paper1/accuracy_map_5_12.png'
 
+    img = img.reshape((256,256,3))
     accu_map = np.zeros((256,256))
     for i in range(label.shape[0]):
         accu= get_pix_acc(preds[:,i,:], label[i,:], loop_num)
@@ -207,7 +211,7 @@ def get_accuracy_map(preds, label, loop_num, img):
 
     #plot_accu_map(img, label, accu_map)
 
-    colors = ['forestgreen', 'lawngreen', 'orange']
+    colors = ['forestgreen','orange']
     colormap = pltc.ListedColormap(colors)
 
     plt.imshow(img)
@@ -305,10 +309,10 @@ def draw_accu_norm_dist(P, std, label, class_num_list = []):
         std = np.sqrt(cluster_means[class_num] * (1-cluster_means[class_num]))
         u = stats.norm.pdf(x, cluster_means[class_num], std)
         if class_num == 0:
-            plot_name = 'tree'
+            plot_name = 'tree+grass'
             color = 'blue'
         elif class_num == 1:
-            plot_name = 'grass'
+            plot_name = 'concrete'
             color = 'orange'
         else:
             plot_name = 'concrete'
@@ -348,6 +352,7 @@ def draw_accu_norm_pixel(P, std, label, class_num):
     Num = 1000
     x = np.linspace(-0.5, 1.7, num=Num)
     x = x.reshape((1000,1))
+
     # y = 1/( np.sqrt( 2*np.pi*b**2 ) ) * np.exp( -1/(2*b**2)*( x - a*np.ones([Num,1]) )**2 )
     # u = stats.norm.pdf(x, a, b)
 
@@ -373,9 +378,9 @@ def plot_accuracy(label, accuracy, std, index):
 
     # change class number to class name for categorical display
     label_line = label_line.astype(str)
-    label_line[label_line == '0'] = "Tree"
-    label_line[label_line == '1'] = "Grass"
-    label_line[label_line == '2'] = "Concrete"
+    label_line[label_line == '0'] = "Tree+Grass"
+    label_line[label_line == '1'] = "Concrete"
+    #label_line[label_line == '2'] = "Concrete"
 
     plt.rcParams["figure.figsize"] = [20, 10]
     # plt.rcParams["figure.autolayout"] = True
@@ -423,9 +428,15 @@ def read_image(image_path):
 
     label = np.array(img_data)
     label = label.reshape((256,256))
+
+    # label = label - 1
+    # if np.max(label)>2:
+    #     label[label > 2] = 2
+
     label = label - 1
-    if np.max(label)>2:
-        label[label > 2] = 2
+    label[label == 1] = 0
+    label[label == 2] = 1
+    label[label == 3] = 1
 
     return label
 
@@ -494,6 +505,7 @@ if __name__ == '__main__':
     im_type = image_path[17:25]
     segment=True
     alpha = 0.5
+    class_num = 2
     unet_option = 'unet_vae_RQ_torch' # options: 'unet_vae_old', 'unet_jaxony', 'unet_vae_RQ_torch', 'unet_vae_RQ_scheme3', 'unet_vae_RQ_scheme1'
     image_option = 'noisy' # "clean" or "noisy"
 
@@ -504,31 +516,34 @@ if __name__ == '__main__':
     image = image.to(device=device, dtype=torch.float32)
 
     if unet_option == 'unet_vae_1':
-        net = UNet_VAE(3)
+        net = UNet_VAE(class_num)
     elif unet_option == 'unet_jaxony':
-        net = UNet_test(3)
+        net = UNet_test(class_num)
     elif unet_option == 'unet_vae_old':
-        net = UNet_VAE_old(3, segment)
+        net = UNet_VAE_old(class_num, segment)
     elif unet_option == 'unet_rq':
-        net = UNet_RQ(3, segment, alpha)
+        net = UNet_RQ(class_num, segment, alpha)
     elif unet_option == 'unet_vae_RQ_old':
-        net = UNet_VAE_RQ_old(3, alpha)
+        net = UNet_VAE_RQ_old(class_num, alpha)
     elif unet_option == 'unet_vae_RQ_allskip_trainable':
-        net = UNet_VAE_RQ_old_trainable(3, alpha)
+        net = UNet_VAE_RQ_old_trainable(class_num, alpha)
     elif unet_option == 'unet_vae_RQ_torch':
-        net = UNet_VAE_RQ_old_torch(3, segment, alpha)
+        net = UNet_VAE_RQ_old_torch(class_num, segment, alpha)
         #net = UNet_VAE_RQ_new_torch(3, segment, alpha)
     elif unet_option == 'unet_vae_RQ_scheme3':
-        net = UNet_VAE_RQ_scheme3(3, segment, alpha)
+        net = UNet_VAE_RQ_scheme3(class_num, segment, alpha)
     elif unet_option == 'unet_vae_RQ_scheme1':
-        net = UNet_VAE_RQ_scheme1(3, segment, alpha)
+        net = UNet_VAE_RQ_scheme1(class_num, segment, alpha)
 
     
     #logging.info(f'Loading model {args.model}')
     logging.info(f'Using device {device}')
 
-    model_unet_jaxony = '/home/geoint/tri/github_files/github_checkpoints/checkpoint_unet_jaxony_4-07_epoch30_0.0_va059_segment.pth'
-    model_unet_vae = '/home/geoint/tri/github_files/github_checkpoints/checkpoint_unet_vae_old_4-05_epoch30_0.0_va059_segment.pth'
+    # model_unet_jaxony = '/home/geoint/tri/github_files/github_checkpoints/checkpoint_unet_jaxony_4-07_epoch30_0.0_va059_segment.pth'
+    # model_unet_vae = '/home/geoint/tri/github_files/github_checkpoints/checkpoint_unet_vae_old_4-05_epoch30_0.0_va059_segment.pth'
+
+    model_unet_jaxony = '/home/geoint/tri/github_files/github_checkpoints/checkpoint_unet_jaxony_epoch11_va059_5-16_segment2class.pth'
+    model_unet_vae = '/home/geoint/tri/github_files/github_checkpoints/checkpoint_unet_vae_old_epoch11_va059_5-16_segment2class.pth'
 
     net.to(device=device)
 
@@ -554,21 +569,21 @@ if __name__ == '__main__':
     # pred_masks = loop_predict(image, net, loop_num, unet_option)
 
     # load pickle file
-    file_pickle_name = '/home/geoint/tri/github_files/unet_vae_RQ_exp2_4-21.pickle'
+    file_pickle_name = '/home/geoint/tri/github_files/unet_vae_RQ_exp2_05-20-2022.pickle'
     with open(file_pickle_name, 'rb') as input_file:
         pred_masks_unetvaerq = pickle.load(input_file)
 
     print("unet vae rq shape: ", pred_masks_unetvaerq.shape)
 
     # load noisy image
-    file_pickle_name = '/home/geoint/tri/github_files/exp2_noisy_im_4-21.pickle'
+    file_pickle_name = '/home/geoint/tri/github_files/exp2_noisy_im_05-20-2022.pickle'
     with open(file_pickle_name, 'rb') as input_file:
         noisy_im = pickle.load(input_file)
 
     # get f_seg for predictions results:
     f_seg_preds = []
     for i in range(loop_num):
-        f_seg_pr = get_f_seg(noisy_im, pred_masks_unetvaerq[i,:,:])
+        f_seg_pr = get_f_seg(noisy_im.cpu().numpy(), pred_masks_unetvaerq[i,:,:], class_num)
         f_seg_preds.append(f_seg_pr)
     f_seg_preds = np.array(f_seg_preds) # 50,256,256,3 # TODO: 
 
@@ -579,13 +594,13 @@ if __name__ == '__main__':
     label_mode = get_label_mode(pred_masks_unetvaerq) # 50,256,256
     # plot_img_and_mask_3(rgb_im, label, f_segmode)
 
-    # save noisy image
+    # save label_mode image
     # file_pickle_name = '/home/geoint/tri/github_files/exp2_label_mode_4-25.pickle'
     # with open(file_pickle_name, 'wb') as handle:
     #     pickle.dump(label_mode, handle, protocol=pickle.HIGHEST_PROTOCOL)
     
 
-    f_seg_mode = get_f_seg(noisy_im, label_mode)
+    f_seg_mode = get_f_seg(noisy_im.cpu().numpy(), label_mode, class_num)
     print("f_segmode_seg: ", f_seg_mode.shape)
     # plot_img_and_mask_5(noisy_im, f_segmode, f_segmode_seg)
 
@@ -594,8 +609,8 @@ if __name__ == '__main__':
 
     #plot_img_and_mask_5(rgb_im, label, mean)
 
-    varmat_pickle_name = '/home/geoint/tri/github_files/unet_vae_RQ_varmat.pickle'
-    # load pickle file
+    varmat_pickle_name = '/home/geoint/tri/github_files/unet_vae_RQ_varmat_5-20.pickle'
+    # save pickle file
     with open(varmat_pickle_name, 'wb') as input_file:
         pickle.dump(var_mat, input_file, protocol=pickle.HIGHEST_PROTOCOL)
 
@@ -606,24 +621,24 @@ if __name__ == '__main__':
 
     #get_red_line_plot(noisy_im, f_seg_preds)
 
-    accuracy_map, accu_std = get_accuracy_map(pred_masks_unetvaerq, label, loop_num, noisy_im)
-    # plot_accuracy(label, accuracy_map, accu_std, index=127)
+    accuracy_map, accu_std = get_accuracy_map(pred_masks_unetvaerq, label, loop_num, tensor_to_jpg(noisy_im))
+    plot_accuracy(label, accuracy_map, accu_std, index=127)
 
-    draw_accu_norm_dist(accuracy_map, accu_std, label, class_num_list = [0,1,2])
+    draw_accu_norm_dist(accuracy_map, accu_std, label, class_num_list = [0,1])
     # draw_accu_norm_dist(accuracy_map, accu_std, label, class_num = 1)
     # draw_accu_norm_dist(accuracy_map, accu_std, label, class_num = 2)
 
     draw_accu_norm_pixel(accuracy_map, accu_std, label, class_num = 0)
     draw_accu_norm_pixel(accuracy_map, accu_std, label, class_num = 1)
-    draw_accu_norm_pixel(accuracy_map, accu_std, label, class_num = 2)
+    # draw_accu_norm_pixel(accuracy_map, accu_std, label, class_num = 2)
 
     det_mat, class_maxdet_index = compute_determinant_covar(label_mode, var_mat)
-    print(det_mat)
-    print(class_maxdet_index)
-    for i in class_maxdet_index.keys():
-        h_ind = class_maxdet_index[i][0][0]
-        w_ind = class_maxdet_index[i][1][0]
-        print("determinant for class "+str(i)+": "+ str(det_mat[h_ind,w_ind]))
-        print(var_mat[h_ind][w_ind])
+    # print(det_mat)
+    # print(class_maxdet_index)
+    # for i in class_maxdet_index.keys():
+    #     h_ind = class_maxdet_index[i][0][0]
+    #     w_ind = class_maxdet_index[i][1][0]
+    #     print("determinant for class "+str(i)+": "+ str(det_mat[h_ind,w_ind]))
+    #     print(var_mat[h_ind][w_ind])
 
     
